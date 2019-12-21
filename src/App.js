5e4237b8b381cb7a575a97ed44d4574f39b726d7
Manan Joshi <mananjoshi1995@gmail.com>
2019-12-21 14:22:57 -0500

    Adding globalstyles from styled-components and resetting a few files.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ChatRoom from './views/ChatRoom';
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
-//  use this method for common icons in different components.
-// library.add(faCheckSquare, faCoffee);
 
 const App = () => {
   return (
---
+++ src/views/AllChats.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AllChats() {
+  return <div />;
+}
+
+export default AllChats;
